4360cfda2c26661472b75fa007e37c38e2121d53
Ebenezer-03 <itzmeebenezer2007@gmail.com>
2025-11-04 11:33:08 +0530

    Matrix Multiplication and addition

---
+++ mat_multiplication.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+matrix_a = np.array([[1, 2, 3], [4, 5, 6]])
+matrix_b = np.array([[7, 8], [10, 11], [12, 13]])
+
+result = np.dot(matrix_a, matrix_b)
+
+print(f"Resultant Matrix:\n{result}")
